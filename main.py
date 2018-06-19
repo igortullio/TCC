cab235855cce7ef39fec1b07f2cb4a7e94ee7eb6
Igor Túllio <igor.goncalves@basis.com.br>
2018-06-19 08:59:10 -0300

    Modificações Barbosa

--- main.py
+++ main.py
@@ -4,30 +4,35 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-
 class Home(Screen):
     pass
-
 
 class Cadastro(Screen):
     pass
 
-
 class Login(Screen):
-    pass
-
+    def login_valido(self):
+        #for l in self.logins:
+        if "a" == self.ids["ti_usuario"].text and "a" == self.ids["ti_senha"].text:
+            print ('login válido')
+            return True
+        print ('login inválido')
+        return False
+        #pass
 
 class ScreenManagement(ScreenManager):
-    pass
-
+    def trocar_tela (self, tela):
+        self.current = tela
+        #pass
 
 class Tela(App):
     def build(self):
         return presentation
 
-
 presentation = Builder.load_file("tela.kv")
-Window.size = 500, 800
+Window.size = 500, 600 #Window.size = 500, 800  #original
 
 if __name__ == "__main__":
     Tela().run()
+
+#: import FadeTransition kivy.uix.screenmanager.FadeTransition

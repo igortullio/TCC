e45fe02e2c418bdcde2166e66b68ba9d9a6b437d
Igor Túllio <igor.goncalves@basis.com.br>
2018-06-29 12:05:20 -0300

    Adição da tela "esqueci a senha" e começo da implementação da tela "mesas"

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.uix.button import Button
-import sqlite3
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button, Label
 
 
 class Home(Screen):
@@ -14,32 +12,51 @@ class Home(Screen):
 class Cadastro(Screen):
 
     def realizar_cadastro(self):
-        Dados().novo(self.ids["ti_usuario"].text, self.ids["ti_senha"].text,
-                     self.ids["ti_email"].text)  # método para cadastrar
+        Dados().novo(self.ids["ti_usuario"].text, self.ids["ti_senha"].text, self.ids["ti_email"].text)  # método para cadastrar
         self.ids["lb_cadastro1"].text = "Cadastro realizado com sucesso"
 
 
 class Mesas(Screen):
 
     def oi(self):
+        j = 1
         for i in range(0, 35):
-            self.ids["outros"].add_widget(Button(text="oi"))
+            if i % 2 == 0:
+                if j < 4:
+                    self.ids["outros"].add_widget(Label(text=""))
+                    j += 1
+                else:
+                    self.ids["outros"].add_widget(Label(text="Mesa"))
+                    if j == 5:
+                        j = 1
+                    else:
+                        j += 1
+            else:
+                self.ids["outros"].add_widget(Button(background_color=(0, 128, 0, 0.7), text="Cadeira"))
+
+    def incrementa(self):
+        global j
+        if j == 5:
+            j = 5
+        else:
+            j += 1
 
 
 class Login(Screen):
 
     def login_valido(self):
-        if Dados().login(self.ids["ti_usuario"].text,
-                         self.ids["ti_senha"].text):  # chamada método verificar login no banco
+        if Dados().login(self.ids["ti_usuario"].text, self.ids["ti_senha"].text):  # chamada método verificar login no banco
             return True
-        # pass
+
+
+class Esqueci(Screen):
+    pass
 
 
 class ScreenManagement(ScreenManager):
 
     def trocar_tela(self, tela):
         self.current = tela
-        # pass
 
 
 class Tela(App):
@@ -56,5 +73,3 @@ if __name__ == "__main__":
 
     Dados().getCon()  # criar banco
     Tela().run()
-
-#: import FadeTransition kivy.uix.screenmanager.FadeTransition
